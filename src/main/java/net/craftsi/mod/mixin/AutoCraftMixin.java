45a346f87c74215c443efb497ddb0c5cfa46add8
needfeer35-glitch <needfeer35@gmail.com>
2026-05-04 23:24:04 +0900

    Update AutoCraftMixin.java

--- src/main/java/net/craftsi/mod/mixin/AutoCraftMixin.java
+++ src/main/java/net/craftsi/mod/mixin/AutoCraftMixin.java
@@ -6,13 +6,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.gui.screen.ingame.CraftingScreen;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.RenderTickCounter;
 
 @Mixin(CraftingScreen.class)
 public class AutoCraftMixin {
 
-    @Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/client/gui/DrawContext;IILnet/minecraft/client/render/RenderTickCounter;)V")
-    private void onOpen(DrawContext context, int mouseX, int mouseY, RenderTickCounter tickCounter, CallbackInfo ci) {
-        // хук для автокрафта при открытии верстака
+    @Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/client/gui/DrawContext;IIF)V")
+    private void onOpen(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
     }
 }
