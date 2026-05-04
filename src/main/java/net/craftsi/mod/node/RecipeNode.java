a448405cdff5df26292568ed4496f3d2987d300e
needfeer35-glitch <needfeer35@gmail.com>
2026-05-04 23:46:31 +0900

    Update RecipeNode.java

--- src/main/java/net/craftsi/mod/node/RecipeNode.java
+++ src/main/java/net/craftsi/mod/node/RecipeNode.java
@@ -5,6 +5,7 @@ import net.minecraft.item.ItemStack;
 public class RecipeNode {
     public String id;
     public int x, y;
+    public int w = 160, h = 100;
     public boolean isActive = true;
     public ItemStack result = ItemStack.EMPTY;
     public ItemStack[] inputs = new ItemStack[9];
