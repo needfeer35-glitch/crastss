cd5850c16fbcb88640ed009edb57e51ac69bbd40
needfeer35-glitch <needfeer35@gmail.com>
2026-05-04 23:44:51 +0900

    Update CraftsiCommand.java

--- src/main/java/net/craftsi/mod/command/CraftsiCommand.java
+++ src/main/java/net/craftsi/mod/command/CraftsiCommand.java
@@ -1,9 +1,9 @@
 package net.craftsi.mod.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import net.craftsi.mod.gui.CraftsiScreen;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.craftsi.mod.gui.CraftsiScreen;
 import net.minecraft.client.MinecraftClient;
 
 public class CraftsiCommand {
@@ -11,10 +11,9 @@ public class CraftsiCommand {
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(
             ClientCommandManager.literal("craftsi")
-                .executes(context -> {
-                    MinecraftClient.getInstance().execute(() ->
-                        MinecraftClient.getInstance().setScreen(new CraftsiScreen())
-                    );
+                .executes(ctx -> {
+                    MinecraftClient mc = MinecraftClient.getInstance();
+                    mc.send(() -> mc.setScreen(new CraftsiScreen()));
                     return 1;
                 })
         );
