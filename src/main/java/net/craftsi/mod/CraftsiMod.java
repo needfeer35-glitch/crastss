ae8fb0f6f4473ddcce77a1ce22c2f60e3b297d62
needfeer35-glitch <needfeer35@gmail.com>
2026-05-04 23:07:15 +0900

    Update CraftsiMod.java

--- src/main/java/net/craftsi/mod/CraftsiMod.java
+++ src/main/java/net/craftsi/mod/CraftsiMod.java
@@ -1,9 +1,11 @@
 package net.craftsi.mod;
 
+import net.craftsi.mod.command.CraftsiCommand;
 import net.craftsi.mod.gui.CraftsiScreen;
 import net.craftsi.mod.logic.CraftExecutor;
 import net.craftsi.mod.node.NodeGraph;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
@@ -16,7 +18,6 @@ public class CraftsiMod implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        // Регистрируем клавишу O
         openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
             "key.craftsi.open_gui",
             InputUtil.Type.KEYSYM,
@@ -24,15 +25,16 @@ public class CraftsiMod implements ClientModInitializer {
             "category.craftsi"
         ));
 
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) ->
+            CraftsiCommand.register(dispatcher)
+        );
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            // Открытие GUI по клавише O
             while (openGuiKey.wasPressed()) {
                 if (client.player != null) {
                     client.setScreen(new CraftsiScreen());
                 }
             }
-
-            // Автокрафт
             if (client.player != null) {
                 CraftExecutor.tick(NodeGraph.getInstance(), client.player);
             }
