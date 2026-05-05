3f846be182ef105183ba41c4ba663f840b93ed47
needfeer35-glitch <needfeer35@gmail.com>
2026-05-05 16:41:29 +0900

    Update RecipeNode.java

--- src/main/java/net/craftsi/mod/node/RecipeNode.java
+++ src/main/java/net/craftsi/mod/node/RecipeNode.java
@@ -5,10 +5,14 @@ import net.minecraft.item.ItemStack;
 public class RecipeNode {
     public String id;
     public int x, y;
-    public int w = 160, h = 100;
+    public int w = 180, h = 110;
     public boolean isActive = true;
+    public boolean autoCraftOn = false;
+    public int priority = 0;
+    public String mode = "Активен"; // Активен / Отключен
     public ItemStack result = ItemStack.EMPTY;
     public ItemStack[] inputs = new ItemStack[9];
+    public int craftCount = 0; // сколько раз скрафтили
 
     public RecipeNode(String id) {
         this.id = id;
